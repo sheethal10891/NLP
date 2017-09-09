29ac09668e875c266a1db564e5ab090261984275
Sheethal Shreedhara <sheethal@sheethals-mbp.home>
2017-09-08 20:35:00 -0400

    Prep for unigram

---
+++ create-sentence.py
@@ -0,0 +1,9 @@
+import preprocessing as ngrams
+
+
+unigram_map = ngrams.unigram_prep('./Project1/SentimentDataset/Dev/neg.txt')
+
+print (unigram_map)
+
+
+
---
+++ preprocessing.py
@@ -0,0 +1,17 @@
+import nltk
+
+
+def createTokens(path):
+    print("abc")
+    lines = open(path).read()
+    tokens = nltk.word_tokenize(lines)
+    words = [w.lower() for w in tokens]
+    return words
+
+def unigram_prep(path):
+    words = createTokens(path);
+    print(words)
+    return sorted(words)
+
+    
+#print(unigram_prep('./Project1/SentimentDataset/Dev/neg.txt'))

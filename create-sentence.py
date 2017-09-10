bc0088e294f4a111e8b3ca2e402582fd99f9ad45
Sheethal Shreedhara <sheethal@sheethals-mbp.home>
2017-09-10 15:31:51 -0400

    added sentence completeion for bigrams and cleaned up code. Added a write up file

--- create-sentence.py
+++ create-sentence.py
@@ -1,17 +1,57 @@
 import preprocessing as ngrams
 import random
+import nltk
+
+global tokens
+tokens = ngrams.createTokens('./Project1/SentimentDataset/Dev/neg.txt');
+
+
+def unigram_sentence(length):
+    
+    unigram_map = ngrams.unigram_prep(tokens)
+    sentence ='';
+    for x in range(length):
+        ran= random.randint(0,len(unigram_map))
+        sentence = sentence + " " +unigram_map[ran]
+    return sentence
+   
+def bigram_sentence(length,complete_sentence=False,sentence=''):
+    bigram_dict= ngrams.get_bigrams(tokens)
+    #bigram_prob_dict = get_biprob(bigram_dict,num_bigrams)
+    start_len=len(sentence)        
+
+    #if no start token given,choose 1 word from the words occuring after '.'(Possible sentence start words)
+    if start_len == 0:
+       result= bigram_dict['.']
+       #sentence = next(iter(bigram_dict))
+       start_token = result[len(result)-1]
+       sentence = start_token;
+    else:
+       sentence_tokens = sentence.split()
+       start_token = sentence_tokens[len(sentence_tokens)-1]
+   #below code is fwhen we use dict from trained corpus
+   # if start_token not in bigram_dict:
+   #    start_token = random.choice(bigram_dict.keys()) 
+        
+        
+   #predict next_words for max count 
+    while (length):
+       next=ngrams.predict_next(bigram_dict,start_token) 
+       sentence = sentence + ' '+ next
+       length=length-1;
+       start_token = next;
+       
+    if (complete_sentence) :
+        while start_token != '.' :
+            next=ngrams.predict_next(bigram_dict,start_token) 
+            sentence = sentence + ' '+ next
+            length=length-1;
+            start_token = next; 
+    return sentence    
 
 
 
+print unigram_sentence(20)
 
-unigram_map = ngrams.unigram_prep('./Project1/SentimentDataset/Dev/neg.txt')
-
-print (len(unigram_map))
-
-for x in range(10):
-  ran= random.randint(0,len(unigram_map))
-  print unigram_map[ran]
-print
-
-
+print bigram_sentence(20, True, "I am who I am")
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,48 @@
 import nltk
-
+import random
 
 def createTokens(path):
-    print("abc")
     lines = open(path).read()
     tokens = nltk.word_tokenize(lines)
     words = [w.lower() for w in tokens]
     return words
 
-def unigram_prep(path):
-    words = createTokens(path);
+def unigram_prep(words):
+    
     #print(words)
     return sorted(words)
-    
-#print(unigram_prep('./Project1/SentimentDataset/Dev/neg.txt'))
+
+def probability_calc(tokens,dictionary,total_tokens):
+    prob_dict={}
+    for token in tokens:
+        prob_dict[token]=dictionary[token]/total_tokens
+    return prob_dict
+
+#Get  the next probable word for a token
+def predict_next(bigram_dict,start_word):
+    ##TODO: write logic to get the word with max probability
+    word_map = bigram_dict[start_word]
+    #print(len(word_map))
+    rn=random.randint(0,len(word_map)-1)
+    #print(" Random")
+    #print(rn)
+    word=word_map[rn]
+    return word
+
+def get_biprob(bigram_dict,num_bigrams):
+     prob_dict={}
+     for token in bigram_dict.keys():
+        for  next in bigram_dict[token].keys():
+	    prob_dict[token][next] = bigram[token][next] /len(bigram[token].keys())    
+     return prob_dict
+
+## Build dictionary of bigrams  
+def get_bigrams(tokens):
+    bigram_dict={}
+    for i in range(0,len(tokens)-1):
+            bigram_dict.setdefault(tokens[i],[]).append(tokens[i+1])
+    return bigram_dict
+
+## Random sentence generator bigrams 
+## Input: max len of sentence,Initial unigram tokens,optional sentence start tokens
+

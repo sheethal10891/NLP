75cbc74e61ce589c63d03cfaf99e5158c88f4dfb
Sheethal Shreedhara <sheethal@sheethals-mbp.home>
2017-10-01 22:12:37 -0400

    precision, recall and fscore

--- Project2/baseline.py
+++ Project2/baseline.py
@@ -1,24 +1,68 @@
-import re
 import csv
 import random
+from decimal import *
 from collections import defaultdict
 
-def createLexicon(path):
-    lines = open(path).readlines();
-    n=len(lines)
-    tags=["B-LOC","B-PER","B-ORG","B-MISC",	"I-LOC","I-PER","I-ORG","I-MISC"]
+
+tags=["B-LOC","B-PER","B-ORG","B-MISC", "I-LOC","I-PER","I-ORG","I-MISC"]
+
+def createLexicon(train):
+    n=len(train)
+    print train[2042]
+    print train[2040]
     lex={}
     for i in xrange(2,n,3):
-        biotags=lines[i].split()
-        words=lines[i-2].split()
+        biotags=train[i].split()
+        words=train[i-2].split()
         for j in range(len(biotags)):
             if biotags[j] in tags:
+                if words[j]==",":
+                    print biotags[j]," ",i
                 lex[words[j]]=biotags[j][2:]
-#    print lex
-    return lex		
+    return lex
+    	
+def calculatePrecision():
+    lex=createLexicon(train)
+    nVal=len(val)
+    correct=0
+    predicted=0
+    totalTags=0
+    for i in range(2,nVal,3):
+        words=val[i-2].split()
+        biotags=val[i].split()
+        for j in range(len(words)):
+            if biotags[j] in tags:
+                totalTags+=1
+                if words[j] in lex:
+                    predicted+=1
+                    tag=lex[words[j]]
+                    if tag==biotags[j][2:]:
+                        correct+=1
+                '''
+                else: # with random words!
+                    predicted+=1
+                    tag=random.choice(tags)
+                    if tag==biotags[j]:
+                        correct+=1
+                        '''
+    print correct
+    print predicted
+    print totalTags
+    precision=Decimal(correct)/Decimal(predicted)
+    recall=Decimal(correct)/Decimal(totalTags)
+    fMeasure=2*precision*recall/(precision+recall)
+    print precision
+    print recall
+    print fMeasure
+  
     
+alllines = open('./train.txt').readlines();
+n=len(alllines)
+train=alllines[:29400]
+val=alllines[29400:] 
+calculatePrecision()
     
-lex=createLexicon('./train.txt');
+#createLexicon('./train.txt');
 
 def test(lex,path):
   
@@ -35,7 +79,7 @@ def test(lex,path):
 	numbers=lines[i+2].split()
         token_len=len(tokens)
 	for j in range(0,token_len-1):
-            if pos[j] in noun_tags:
+        if pos[j] in noun_tags:
 	       if tokens[j] in lex: 
 		   tag=lex[tokens[j]]
 		   tag_dict[tag].append(int(numbers[j]))
@@ -69,4 +113,4 @@ def test(lex,path):
            w.writerow([key]+ value)
         file.close()
 
-test(lex,'test.txt')
+#test(lex,'test.txt')
